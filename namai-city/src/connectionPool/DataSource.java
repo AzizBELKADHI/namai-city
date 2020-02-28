0edf8a08ac071992041ababd7e598f937c67a250
idirh <idirh@10.10.28.92>
2020-02-28 14:11:26 +0100

    implementation DataSource

--- namai-city/src/connectionPool/DataSource.java
+++ namai-city/src/connectionPool/DataSource.java
@@ -1,5 +1,24 @@
 package connectionPool;
 
+import java.sql.*;
+
 public class DataSource {
+	private static JDBCConnectionPool connectionPool;
+	public DataSource() throws ClassNotFoundException, SQLException {
+		connectionPool = new JDBCConnectionPool();
+	}
+	
+	public static Connection getConnection() {
+		return connectionPool.getConnection();
+	}
+	
+	public static void addConnection(Connection c) {
+		connectionPool.addConnection(c);
+	}
+	
+	public static void closeConnections() throws SQLException {
+		connectionPool.closeConnections();
+	}
+	
 
 }
